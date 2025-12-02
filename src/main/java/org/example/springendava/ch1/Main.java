4e6e55eee9a845479f907d27e73eb69df9c2f9c7
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-02 16:14:49 +0200

    Register beans functionally and using annotations

---
+++ src/main/java/org/example/springendava/ch1/Main.java
@@ -0,0 +1,28 @@
+package org.example.springendava.ch1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        // eroare daca pun parrot 1
+        context.registerBean("parrot5", Parrot.class, Parrot::new);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p);
+        System.out.println(p.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
+
+    }
+}
---
+++ src/main/java/org/example/springendava/ch1/ProjectConfig.java
@@ -0,0 +1,43 @@
+package org.example.springendava.ch1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+// @ComponentScan
+public class ProjectConfig {
+
+    @Bean
+    @Primary
+    Parrot parrot1() {
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean(name = "miki")
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean(value = "riki")
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Riki");
+        return p;
+    }
+
+    @Bean
+    String hello() {
+        return "Hello";
+    }
+
+    @Bean
+    Integer ten() {
+        return 10;
+    }
+
+}
