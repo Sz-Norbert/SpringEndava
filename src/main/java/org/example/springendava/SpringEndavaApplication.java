06239a074125b6b0b3c5f281fd9a20749a1c0fbe
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-02 16:07:56 +0200

    first commit

---
+++ src/main/java/org/example/springendava/SpringEndavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.springendava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEndavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEndavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springendava/SpringEndavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springendava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEndavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
