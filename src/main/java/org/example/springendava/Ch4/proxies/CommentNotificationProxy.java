6253a17ec7a2391465641fa054f1be943673b9d4
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-02 17:02:00 +0200

    Implement dependency injection with abstractions, on interface implemented by multiple classes

---
+++ src/main/java/org/example/springendava/Ch4/proxies/CommentNotificationProxy.java
@@ -0,0 +1,10 @@
+package org.example.springendava.Ch4.proxies;
+
+
+import org.example.springendava.Ch4.Comment;
+
+public interface CommentNotificationProxy {
+
+    void sendComment(Comment comment);
+
+}
