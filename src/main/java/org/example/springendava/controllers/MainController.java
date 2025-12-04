de3d342189795c1fb51e6571cfed6ed5d22f37fb
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-04 12:00:52 +0200

    implemented simple mvc hello app.

---
+++ src/main/java/org/example/springendava/controllers/MainController.java
@@ -0,0 +1,15 @@
+package org.example.springendava.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping("/home")
+    public String home(){
+        return "homePage.html";
+    }
+
+}
