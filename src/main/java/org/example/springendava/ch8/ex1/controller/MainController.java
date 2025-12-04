73e8b0dc912edd58117c8c7603460481e2e31142
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-04 13:28:10 +0200

    implemented simple mvc  hello app with Thymeleaf engine and using RequestParam.

---
+++ src/main/java/org/example/springendava/ch8/ex1/controller/MainController.java
@@ -0,0 +1,20 @@
+package org.example.springendava.ch8.ex1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home(
+            @RequestParam String color,
+            @RequestParam String name,
+            Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color",color  );
+        return "home.html";
+    }
+}
