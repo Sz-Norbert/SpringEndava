6ac3c5fef75cd83858450e1ff63b3c3600d28232
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-08 10:56:03 +0200

    Implemented Country endpoint and worked with headers and ResponseEntity

---
+++ src/main/java/org/example/springendava/models/Country.java
@@ -0,0 +1,19 @@
+package org.example.springendava.models;
+
+import lombok.Data;
+
+@Data
+public class Country {
+
+
+    private String name;
+    private int population;
+
+
+    public static Country of(String name,  int population) {
+        Country country = new Country();
+        country.setName(name);
+        country.setPopulation(population);
+        return country;
+    }
+}
