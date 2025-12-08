d9f739f250a1f26b92300a897a9ca31e3bb140ce
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-08 10:25:31 +0200

    implemented ch10, ex1, RestController

---
+++ src/main/java/org/example/springendava/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package org.example.springendava.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/Hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() {
+        return "Ciao!";
+    }
+
+
+}
