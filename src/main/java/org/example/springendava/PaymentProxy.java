51e4fdbc97b55cd8449db49212afb7431978251a
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-08 14:31:35 +0200

    fixed

--- src/main/java/org/example/springendava/PaymentProxy.java
+++ src/main/java/org/example/springendava/PaymentProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "payments", url = "${name.service.url}")
-public interface OpenFeignClient {
+public interface PaymentProxy {
 
     @PostMapping("/payment")
     Payment createPayment(@RequestHeader String requestId, @RequestBody Payment payment);
--- src/main/java/org/example/springendava/controllers/PaymentController.java
+++ src/main/java/org/example/springendava/controllers/PaymentController.java
@@ -1,13 +1,10 @@
 package org.example.springendava.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.example.springendava.OpenFeignClient;
+import org.example.springendava.PaymentProxy;
 import org.example.springendava.models.Payment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -19,14 +16,15 @@ public class PaymentController {
 
     private static Logger logger =Logger.getLogger(PaymentController.class.getName());
 
-    private final OpenFeignClient paymentsProxy;
+    private final PaymentProxy paymentsProxy;
 
     @PostMapping("/payment")
     public Payment createPayment(
             @RequestBody Payment payment
     ) {
         String requestId = UUID.randomUUID().toString();
-        return paymentsProxy.createPayment(requestId, payment);
+        logger.info("Processing payment with requestId: " + requestId);
+        return paymentsProxy.createPayment(requestId,payment);
     }
 
 
