23005222b56bb33feb3b1841b613eeb0e571d0af
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-08 11:46:11 +0200

    Implemented RequestBody for make payment post method.

--- src/main/java/org/example/springendava/controller/PaymentController.java
+++ src/main/java/org/example/springendava/controller/PaymentController.java
@@ -13,16 +13,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequiredArgsConstructor
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
 
     @PostMapping("/payment")
-    public ResponseEntity<PaymentDetails> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+
+        logger.info("Received payment " +
+                paymentDetails.getAmount());
+
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
