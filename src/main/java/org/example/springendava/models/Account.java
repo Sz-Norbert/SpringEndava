6b787dec25ca7c6a375e89c5c5f5ca7e11b4e141
Sz-Norbert <szasznikanorbert676@gmail.com>
2025-12-14 16:56:16 +0200

    Write Unit tests and integration

--- src/main/java/org/example/springendava/models/Account.java
+++ src/main/java/org/example/springendava/models/Account.java
@@ -1,13 +1,17 @@
 package org.example.springendava.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
 
     @Id
